a1a1df70c5c950fa47e51ab63a287d0c21bd4078
Allam Chaves Zamora <allam@una.cr>
2023-05-15 13:23:46 -0600

    ajuste de acomodo

--- app/src/main/java/cr/ac/una/controlarterial/viewModel/TomaArterialViewModel.kt
+++ app/src/main/java/cr/ac/una/controlarterial/viewModel/TomaArterialViewModel.kt
@@ -13,9 +13,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 class TomaArterialViewModel: ViewModel (){
     private var _tomasArteriales: MutableLiveData<List<TomaArterial>> = MutableLiveData()
     var tomasArteriales: LiveData<List<TomaArterial>> = _tomasArteriales
+    lateinit var  apiService : TomaArterialDAO
 
 
     suspend fun listTomaArterial() {
+        intService()
+        var lista = apiService.getItems()
+        _tomasArteriales.postValue(lista.items)
+
+    }
+    fun intService(){
         val client = OkHttpClient.Builder()
             .addInterceptor(AuthInterceptor("U1xIqs39A09cmfpUSpP_Mis54d1ElzUfQx_oyB7wuCccfEkDBQ"))
             .build()
@@ -24,12 +31,6 @@ class TomaArterialViewModel: ViewModel (){
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        val apiService = retrofit.create(TomaArterialDAO::class.java)
-        var lista = apiService.getItems()
-        _tomasArteriales.postValue(lista.items)
-
+        apiService = retrofit.create(TomaArterialDAO::class.java)
     }
-
-
-
 }
